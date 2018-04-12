70b403c76fbfb5adb1adfec12157e7a0529e1ee9
須永勘太 <sunagakannta6@gmail.com>
2018-04-12 09:58:56 +0900

    feat: 値段をとってくるメソッド完成

--- app/models/stok.rb
+++ app/models/stok.rb
@@ -2,12 +2,10 @@ class Stok < ApplicationRecord
   belongs_to :item
   has_many :cron_infos
 
-  def self.update_all_stocks
-    Stok.all.each do |s|
-      doc = Nokogiri::HTML(open(s.url))
-      price_str = doc.xpath(s.position).text
-      price_int = price_str.slice(/[!^0-9]+/).to_i
-      s.cron_infos.create(price: price_int)
-    end
+  def update_all_stocks
+    doc = Nokogiri::HTML(open(self.url))
+    price_str = doc.xpath(self.position).text
+    price_int = price_str.slice(/[!^0-9]+/).to_i
+    s.cron_infos.create(price: price_int)
   end
 end
